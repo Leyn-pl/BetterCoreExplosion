ba782a1f5e6e74946c6d60af8431cd5fb8914d02
Leyn <65331501+Leyn-pl@users.noreply.github.com>
2025-03-21 19:14:03 +0300

    Updated by afflict

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 Events.on(CoreChangeEvent, (data) => {
-	// FOR SOME UNKNOWN TO MANKIND REASON, core.health does not work for triggering it on core death in multiplayer. So here is cut version of my idea
-	LogicFx.get("crossExplosion").effect.at(data.core.x, data.core.y, data.core.hitSize()*5, data.core.team.color);
+    Timer.schedule(() => {
+        if (data.core.tile.block() instanceof CoreBlock) {
+            return;
+        }
+        LogicFx.get("crossExplosion").effect.at(data.core.x, data.core.y, 175, data.core.team.color);
+    }, 0.2)
 });
